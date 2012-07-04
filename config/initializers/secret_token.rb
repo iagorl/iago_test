73ccec8360627cf92cd41f36b9307e73a9c528fa
iago <iago@iago-desktop.(none)>
2012-07-04 13:49:49 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IagoTest::Application.config.secret_token = 'a7c0d61d51c138ecd0d76684fdd2b903655d00d32f1cb45c468d9358903ed665f10309351023866e5542271adad4bc78242881f92c7596c078499272a3ed9f2a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IagoTest::Application.config.session_store :cookie_store, key: '_iago_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IagoTest::Application.config.session_store :active_record_store
